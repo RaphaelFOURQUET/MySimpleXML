c09658d66e3c2036c2af2f7397c20cdfc3766b6b
RaphaelFOURQUET <raphikifourque@gmail.com>
2013-02-27 15:38:56 +0100

    #1 Commentaire

---
+++ MySimpleXML/src/main/java/dcll/rfou/MySimpleXML/App.java
@@ -0,0 +1,17 @@
+package dcll.rfou.MySimpleXML;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+/**
+ * main classique
+ * @param args
+ */
+    public static void main(final String[] args) {
+    	//Un premier commentaire !
+        System.out.println("Hello World!");
+        System.out.println("test");
+    }
+}
